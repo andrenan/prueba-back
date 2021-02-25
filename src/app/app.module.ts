2614f55644bf4f06432040e51afb4a463a3c87de
andrenan <andre_2297@hotmail.es>
2021-02-25 15:35:41 -0500

    prueba backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home' , component: HomeComponent},
+  { path: 'about' , component: AboutComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileUploadModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
